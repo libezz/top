870c3af05aee663ae4c3d32c3d2b49f327869ff6
libezz <libezz@foxmail.com>
2018-03-16 18:33:10 +0800

    添加基础枚举和工具类

---
+++ src/main/java/top/type/SortByType.java
@@ -0,0 +1,38 @@
+package top.type;
+
+public enum SortByType {
+
+	ID(0, "id"),
+	CREATE_DATE(1, "create_date"),
+	LAST_MODIFIED_DATE(2, "last_modified_date");
+	
+	private int value;
+	private String sortBy;
+	
+	private SortByType(int value, String sortBy) {
+		this.value = value;
+		this.sortBy = sortBy;
+	}
+	
+	public static SortByType getType(int value) {
+		for(SortByType type : values()) {
+			if(type.getValue() == value) {
+				return type;
+			}
+		}
+		return null;
+	}
+
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public String getSortBy() {
+		return sortBy;
+	}
+	public void setSortBy(String sortBy) {
+		this.sortBy = sortBy;
+	}
+}
---
+++ src/main/java/top/utils/RoleUtils.java
@@ -0,0 +1,15 @@
+package top.utils;
+
+import top.type.URIGroupType;
+
+public class RoleUtils {
+
+	public static boolean isRole(Long sumRole, URIGroupType group) {
+		if(group.getNeedRole() == null || sumRole.equals(-1000L)) {
+			return true;
+		}
+		Long temp = Long.divideUnsigned(sumRole, group.getNeedRole());
+		Long result = Long.remainderUnsigned(temp, 2L);
+		return result.equals(1L);
+	}
+}
