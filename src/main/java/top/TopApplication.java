233944834ac3fe824b9d67d5e6af92f5b3349173
libezz <libezz@foxmail.com>
2018-03-16 17:25:24 +0800

    版本0.0.0

---
+++ src/main/java/top/TopApplication.java
@@ -0,0 +1,24 @@
+package top;
+
+import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class TopApplication {
+
+	private final Logger logger = LoggerFactory.getLogger(TopApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TopApplication.class, args);
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		logger.warn("=================【server start】=================");
+	}
+}
